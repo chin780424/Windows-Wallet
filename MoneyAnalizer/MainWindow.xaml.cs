aa7a7bf50ba0654e7f7c59535594678342bc291c
chin780424 <czerneckip1@gmail.com>
2017-12-28 22:13:54 +0100

    Refaktoryzacja funkcji zmiany awatara

--- MoneyAnalizer/MainWindow.xaml.cs
+++ MoneyAnalizer/MainWindow.xaml.cs
@@ -20,105 +20,80 @@ namespace MoneyAnalizer
     /// </summary>
     public partial class MainWindow : Window
     {
-
         int i = 0;
 
         private void ChangeAwatar(int j)
         {
             BitmapImage User = new BitmapImage();
+            User.BeginInit();
             switch (j)
             {
                 case 1:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 2:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_women2.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 3:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_waiter2.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 4:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_women.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 5:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_accountant.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 6:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_cook.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 7:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_doctor.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 8:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_girl.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 9:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_girl2.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 10:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_grandpa.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 11:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_solider.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 12:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_thief.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
 
                 case 13:
-                    User.BeginInit();
                     User.UriSource = new Uri(@"icons/user_waiter.png", UriKind.Relative);
-                    User.EndInit();
                     img_avatar.Source = User;
                     break;
             }
+            User.EndInit();
         }
 
         public MainWindow()
@@ -126,8 +101,10 @@ namespace MoneyAnalizer
             InitializeComponent();
             i = Properties.Settings.Default.AwatarID;
             ChangeAwatar(i);
-        }
 
+            ChangeFirstName window = new ChangeFirstName();
+            window.Show();
+        }
 
         int k = 0;
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
