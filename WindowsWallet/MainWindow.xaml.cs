d3f6d22b8b88822d0cf9a2aa24fc42d39f16791e
chin780424 <czerneckip1@gmail.com>
2018-02-06 15:35:21 +0100

    refactored ChangeAvatar function

--- WindowsWallet/MainWindow.xaml.cs
+++ WindowsWallet/MainWindow.xaml.cs
@@ -29,73 +29,10 @@ namespace MoneyAnalizer
         {
             BitmapImage User = new BitmapImage();
             User.BeginInit();
-            switch (j)
-            {
-                case 1:
-                    User.UriSource = new Uri(@"icons/user.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
 
-                case 2:
-                    User.UriSource = new Uri(@"icons/user_women2.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
+            User.UriSource = new Uri(@"icons/user" + j + ".png", UriKind.Relative);
 
-                case 3:
-                    User.UriSource = new Uri(@"icons/user_waiter2.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 4:
-                    User.UriSource = new Uri(@"icons/user_women.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 5:
-                    User.UriSource = new Uri(@"icons/user_accountant.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 6:
-                    User.UriSource = new Uri(@"icons/user_cook.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 7:
-                    User.UriSource = new Uri(@"icons/user_doctor.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 8:
-                    User.UriSource = new Uri(@"icons/user_girl.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 9:
-                    User.UriSource = new Uri(@"icons/user_girl2.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 10:
-                    User.UriSource = new Uri(@"icons/user_grandpa.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 11:
-                    User.UriSource = new Uri(@"icons/user_solider.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 12:
-                    User.UriSource = new Uri(@"icons/user_thief.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-
-                case 13:
-                    User.UriSource = new Uri(@"icons/user_waiter.png", UriKind.Relative);
-                    img_avatar.Source = User;
-                    break;
-            }
+            img_avatar.Source = User;
             User.EndInit();
         }
 
